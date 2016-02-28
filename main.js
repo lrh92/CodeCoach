e111a2c2dfa517f8f3c720e28036dbdc22899b94
lrh92 <laurahyde@btinternet.com>
2016-02-28 19:56:28 +0000

    stylesheet changes and image addition

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+$(document).ready(function() {
+	function scrollToAnchor(aid){
+		var aTag = $("a[name='"+ aid +"']");
+		$('html,body').animate({scrollTop: aTag.offset().top},'slow');
+	}
+	$("a").click(function() {
+		var href = $(this).attr('href').replace('#', '')
+		scrollToAnchor(href);
+	});			
 });
