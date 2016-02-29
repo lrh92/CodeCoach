43bbaa749f52cc852d1a4112bb975f51bc3b6ec2
georgiaobrien <georgiacobrien@gmail.com>
2016-02-29 18:50:57 +0000

    Fix all indenting

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ $(document).on("scroll", function() {
 });
 
 $(document).ready(function() {
-  function scrollToAnchor(aid){
-    var aTag = $("a[name='"+ aid +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-  }
-  $("a").click(function() {
-    var href = $(this).attr('href').replace('#', '')
-    scrollToAnchor(href);
-  });     
+    function scrollToAnchor(aid) {
+        var aTag = $("a[name='" + aid + "']");
+        $('html,body').animate({ scrollTop: aTag.offset().top }, 'slow');
+    }
+    $("a").click(function() {
+        var href = $(this).attr('href').replace('#', '')
+        scrollToAnchor(href);
+    });
 });
 
-                $("form :input").each(function(index, elem) {
-                var eId = $(elem).attr("id");
-                var label = null;
-                if (eId && (label = $(elem).parents("form").find("label[for=" + eId + "]")).length == 1) {
-                $(elem).attr("placeholder", $(label).text());
-                $(label).hide();
-                }
-                });
+$("form :input").each(function(index, elem) {
+    var eId = $(elem).attr("id");
+    var label = null;
+    if (eId && (label = $(elem).parents("form").find("label[for=" + eId + "]")).length == 1) {
+        $(elem).attr("placeholder", $(label).text());
+        $(label).hide();
+    }
+});
