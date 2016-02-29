f5f3a106266e2ca86415357f7c5c25110568b59e
georgiaobrien <georgiacobrien@gmail.com>
2016-02-29 17:42:14 +0000

    New images, reorganise files, make left right instead of centered

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ $(document).on("scroll", function() {
     }
 
 });
+
+$(document).ready(function() {
+  function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+  }
+  $("a").click(function() {
+    var href = $(this).attr('href').replace('#', '')
+    scrollToAnchor(href);
+  });     
+});
+
+                $("form :input").each(function(index, elem) {
+                var eId = $(elem).attr("id");
+                var label = null;
+                if (eId && (label = $(elem).parents("form").find("label[for=" + eId + "]")).length == 1) {
+                $(elem).attr("placeholder", $(label).text());
+                $(label).hide();
+                }
+                });
